5f8fac566212b9047e7f8f74d6e38a9ea72efd68
azizmoduit <abdul.aziz@moduit.id>
2024-10-31 21:51:14 +0700

    test commit

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,8 +2,10 @@
 
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import type { ThemeProviderProps } from "next-themes/dist/types"
-import * as React from "react"
+import * as React from "react";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) 
+{
+
 	return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
