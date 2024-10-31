6d01c8ae93599992bfc441c94b227214fe4f8c2a
azizmoduit <abdul.aziz@moduit.id>
2024-10-31 21:49:23 +0700

    test

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,8 @@
-export type SiteConfig = typeof siteConfig
+export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
 	name: "Dynamic Form Fields",
+
 	links: {
 		github: "https://github.com/azizvector/",
 		linkedin: "https://www.linkedin.com/in/abdul-aziz-b374541bb/",
